9bfec78cc18086017bc01b51b89f06c43476901a
tayyab_20 <tayyabali78013@gmail.com>
2024-05-28 22:47:27 +0500

    first commit

--- bio-data.py
+++ bio-data.py
@@ -2,41 +2,63 @@
 # Section, Percentage and grade of matriculation and Intermediate. All the fields should
 # be entered from the console at run time.
 
-greetingsMessage = str(input('Greetings! Hopefully you are doing good Buddy. Press enter to continue.'))
+input('Greetings! Hopefully you are doing good Buddy. Press enter to continue.')
 
-botInformationMessage = str(input("I'm a builtin scripted bot. My job is to generate your Bio Data based on your inputs. Press enter to continue."))
+input("I'm a built-in scripted bot. My job is to generate your Bio Data based on your inputs. Press enter to continue.")
 
-welcomeMessage = str(input('Welcome to our Bio Data Generator! Press enter to continue.'))
+input('Welcome to our Bio Data Generator! Press enter to continue.')
 
-studentName = str(input('Please enter your name here: '))
+studentName = input('Please enter your name here: ')
 studentAge = int(input('Please enter your age here: '))
 
-studentUniversityName = str(input('Please enter your university name here: '))
+studentUniversityName = input('Please enter your university name here: ')
 studentRollNo = int(input('Please enter your roll no here: '))
-studentTechnology = str(input('Please enter your technology name here: '))
-studentSection = str(input('Please enter your section here: '))
+studentTechnology = input('Please enter your technology name here: ')
+studentSection = input('Please enter your section here: ')
 
-smartScript1 = str(input(
-    "Now you don't have to write all your subjects name, our smartly written script will automatically generates a list and you can proceed without any delay. Press enter to continue."))
+input("Now you don't have to write all your subjects name, our smartly written script will automatically generate a list and you can proceed without any delay. Press enter to continue.")
 
-python = 'Python'
-ict = 'ICT'
-english = 'English'
-calculus = 'Calculus'
-physics = 'Physics'
+subjects = ['Python', 'ICT', 'English', 'Calculus', 'Physics']
 
-alert1 = str(input('Please fill all the correct matriculation credentials. Press enter to continue.'))
+questionAboutWebDevelopment = input('Say yes if you are interested in Web Development and our smart script will automatically generate a list of all latest tech languages and frameworks so, that you can just pick up all the ones you have a command over and simply add them to your Bio Data.')
+
+languages = [
+    'HTML5', 'CSS3', 'Bootstrap', 'JavaScript', 'React.js', 'Python', 
+    'C', 'C++', 'C#', 'Java', 'React Native', 'Swift', 'Kotlin', 'MySQL', 
+    'PHP', 'OOP'
+]
+
+selected_languages = []
+
+print('Here is the list of available languages and frameworks:')
+
+for i, language in enumerate(languages, 1):
+    print(f'{i}. {language}')
+
+while True:
+    
+    choice = input('Enter the number of the language you want to add to your bio, or type "exit" to finish: ')
+    
+    if choice.lower() == 'exit':
+        break
+    
+    elif choice.isdigit() and 1 <= int(choice) <= len(languages):
+        selected_languages.append(languages[int(choice) - 1])
+    
+    else:
+        print('Invalid input. Please enter a valid number or "exit".')
+
+input('Please fill all the correct matriculation credentials. Press enter to continue.')
 
 studentMatriculationTotalMarks = int(input('Please enter your total marks of matriculation board here: '))
 studentMatriculationObtainedMarks = int(input('Please enter your obtained marks of matriculation board here: '))
 
-alert2 = str(input('Please fill all the correct intermediate credentials. Press enter to continue.'))
+input('Please fill all the correct intermediate credentials. Press enter to continue.')
 
 studentIntermediateTotalMarks = int(input('Please enter your total marks of intermediate board here: '))
 studentIntermediateObtainedMarks = int(input('Please enter your obtained marks of intermediate board here: '))
 
-smartScript2 = str(input(
-    'Our smart script will automatically calculates your both matric and intermediate percentages so, you dont have to worry at all. Press enter to continue.'))
+input('Our smart script will automatically calculate your both matric and intermediate percentages so, you don’t have to worry at all. Press enter to continue.')
 
 # Calculating Matriculation percentage
 studentMatriculationPercentage = studentMatriculationObtainedMarks / studentMatriculationTotalMarks * 100
@@ -44,67 +66,57 @@ studentMatriculationPercentage = studentMatriculationObtainedMarks / studentMatr
 # Calculating Intermediate percentage
 studentIntermediatePercentage = studentIntermediateObtainedMarks / studentIntermediateTotalMarks * 100
 
-smartScript3 = str(input(
-    'Grades will automatically be detect based on your matric and intermediate percentages. Press enter to continue.'))
+input('Grades will automatically be detected based on your matric and intermediate percentages. Press enter to continue.')
 
-# Determine the grade for matriculation percentage
+# Determining the grades
+def determine_grade(percentage):
+    if 80 <= percentage <= 100:
+        return 'A+'
+    elif 70 <= percentage < 80:
+        return 'A'
+    elif 60 <= percentage < 70:
+        return 'B'
+    elif 50 <= percentage < 60:
+        return 'C'
+    elif 40 <= percentage < 50:
+        return 'D'
+    elif 30 <= percentage < 40:
+        return 'E'
+    elif 20 <= percentage < 30:
+        return 'F'
+    elif 10 <= percentage < 20:
+        return 'G'
+    else:
+        return 'Fail'
 
-if 80 <= studentMatriculationPercentage <= 100:
-    studentMatriculationGrade = 'A+'
-elif 70 <= studentMatriculationPercentage < 80:
-    studentMatriculationGrade = 'A'
-elif 60 <= studentMatriculationPercentage < 70:
-    studentMatriculationGrade = 'B'
-elif 50 <= studentMatriculationPercentage < 60:
-    studentMatriculationGrade = 'C'
-elif 40 <= studentMatriculationPercentage < 50:
-    studentMatriculationGrade = 'D'
-elif 30 <= studentMatriculationPercentage < 40:
-    studentMatriculationGrade = 'E'
-elif 20 <= studentMatriculationPercentage < 30:
-    studentMatriculationGrade = 'F'
-elif 10 <= studentMatriculationPercentage < 20:
-    studentMatriculationGrade = 'G'
-else:
-    studentMatriculationGrade = 'Sorry to say but you are fail.'
+studentMatriculationGrade = determine_grade(studentMatriculationPercentage)
+studentIntermediateGrade = determine_grade(studentIntermediatePercentage)
 
-# Determine the grade for intermediate percentage
-if 80 <= studentIntermediatePercentage <= 100:
-    studentIntermediateGrade = 'A+'
-elif 70 <= studentIntermediatePercentage < 80:
-    studentIntermediateGrade = 'A'
-elif 60 <= studentIntermediatePercentage < 70:
-    studentIntermediateGrade = 'B'
-elif 50 <= studentIntermediatePercentage < 60:
-    studentIntermediateGrade = 'C'
-elif 40 <= studentIntermediatePercentage < 50:
-    studentIntermediateGrade = 'D'
-elif 30 <= studentIntermediatePercentage < 40:
-    studentIntermediateGrade = 'E'
-elif 20 <= studentIntermediatePercentage < 30:
-    studentIntermediateGrade = 'F'
-elif 10 <= studentIntermediatePercentage < 20:
-    studentIntermediateGrade = 'G'
-else:
-    studentIntermediateGrade = 'Sorry to say but you are fail.'
-    
-smartScript4 = str(input('Loading...\n Your bio data is generated.\n Press enter to continue.'))
+input('Loading...\n Your bio data is generated.\n Press enter to continue.')
 
 # Printing the student bio data
 
 print('BIO DATA')
+
 print('According to your given information your bio data involves the following:')
-
-print('Your name is',studentName)
+print('Your name is', studentName)
 print('Your age is', studentAge)
-print('Your roll no is',studentRollNo)
-print('Your technology is',studentTechnology)
-print('Your section is',studentSection)
+print('Your roll no is', studentRollNo)
+print('Your technology is', studentTechnology)
+print('Your section is', studentSection)
+print('Your subjects of BSC Software Technology Are:')
 
-print(f'Your subjects of BSC Software Technology Are:- \n 1) {python} \n 2) {ict} \n 3) {calculus} \n 4) {physics} \n 5) {english}')
+for i, subject in enumerate(subjects, 1):
+    print(f' {i}) {subject}')
 
-print(f'Your percentage is {studentMatriculationPercentage:.2f}%')
-print(f'Your percentage is {studentIntermediatePercentage:.2f}%')
+print('Your selected tech languages are:')
 
-print('Your matriculation grade is',studentMatriculationGrade)
-print('Your intermediate grade is',studentIntermediateGrade)
+for i, language in enumerate(selected_languages, 1):
+    print(f' {i}. {language}')
+
+print(f'Your matriculation percentage is {studentMatriculationPercentage:.2f}%')
+print(f'Your intermediate percentage is {studentIntermediatePercentage:.2f}%')
+print('Your matriculation grade is', studentMatriculationGrade)
+print('Your intermediate grade is', studentIntermediateGrade)
+
+print('The process of generating your Bio Data has been completed successfully!')
