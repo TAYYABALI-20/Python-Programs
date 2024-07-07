37e577b290e88b56600af12fcd55435c4287b024
tayyab_20 <tayyabali78013@gmail.com>
2024-07-08 00:52:47 +0500

    first commit

--- patterns-tutorial.py
+++ patterns-tutorial.py
@@ -18,9 +18,9 @@ if answerQuestion1 == 'yes':
     
     print('\nPattern # 5 => Right-Aligned Right-Angled Triangle: \n    * \n   ** \n  *** \n **** \n*****')
     
-    print('\nPattern # 7 => Diamond Triangle: \n    * \n   *** \n  ***** \n ******* \n********* \n********* \n ******* \n  ***** \n   *** \n    *')
+    print('\nPattern # 6 => Diamond Triangle: \n    * \n   *** \n  ***** \n ******* \n********* \n********* \n ******* \n  ***** \n   *** \n    *')
     
-    answerUestion2 = int(input('So, in order to learn any of the above Pattern just write a number between 1 - 1 (2 till 7 are in process): '))
+    answerUestion2 = int(input('So, in order to learn any of the above Pattern just write a number between 1 - 1 (2 till 6 are in process): '))
     
     if answerUestion2 == 1:
         
@@ -58,17 +58,17 @@ if answerQuestion1 == 'yes':
                                 
                                 if answerUestion9 == 'print()':
                                     
-                                    demoOfPrintStatement = "print('' * (5-i) + '*' * (2 * i - 1))"
+                                    demoOfPrintStatement = "print(' ' * (5-i) + '*' * (2 * i - 1))"
                                     
                                     print(f'\nLet me show you a quick demo of the logic inside print statement. \n\n=> {demoOfPrintStatement}')
                                 
                                     print("\nLet's go through with the logic inside print statement every step of the way:")
                                     
-                                    print(f"\n=> '' * (5-i) => This part will create spaces, the number of spaces decreases as 'i' inside for loop increase. \n\nFor example: The 'i' will be 1 in the first iteration and a single '*' will be print. \n\nNow '' => (spaces) * (5 - i) => where '5' is the total number of rows since we have set the range from '{answerUestion7}'. \n\nNow our output contains '*' so, we have to add some spaces before the '*' and this behaviour can be achieve using \n\n(5 - i) means '4', we have to add 4 spaces before the '*' to make the shape of Equilateral-Triangle. \n\nIn the second iteration the 'i' will be '2' so (5 - 2) means '3' we have to add 3 spaces before '*', and so on this \n\nprocess will be continue until the space is 0 means (5 - 5) and this will be done in the last iteration. \n\nFor example: \n=> ' ' * (5-1) =>     4 Spaces \n=> ' ' * (5-2) =>    3 Spaces \n=> ' ' * (5-3) =>   2 Spaces \n=> ' ' * (5-4) =>  1 Space \n=> ' ' * (5-5) => 0 Space")
+                                    print(f"\n=> '' * (5-i) => This part will create spaces, the number of spaces decreases as 'i' inside for loop increase. Giving a space in empty Strings means that by default we want to give the stars a single space so the additional spaces will be increase when i is 1 and spaces will be decrease as long as i increases. \n\nFor example: The 'i' will be 1 in the first iteration and a single '*' will be print. \n\nNow '' => (spaces) * (5 - i) => where '5' is the total number of rows since we have set the range from '{answerUestion7}'. \n\nNow our output contains '*' so, we have to add some spaces before the '*' and this behaviour can be achieve using \n\n(5 - i) means '4', we have to add 4 spaces before the '*' to make the shape of Equilateral-Triangle. \n\nIn the second iteration the 'i' will be '2' so (5 - 2) means '3' we have to add 3 spaces before '*', and so on this \n\nprocess will be continue until the space is 0 means (5 - 5) and this will be done in the last iteration. \n\nFor example: \n=> ' ' * (5-1) =>     4 Spaces \n=> ' ' * (5-2) =>    3 Spaces \n=> ' ' * (5-3) =>   2 Spaces \n=> ' ' * (5-4) =>  1 Space \n=> ' ' * (5-5) => 0 Space")
                                     
                                     print("\n=> '*' * (2 * i - 1) => This part creates the stars, the number of stars increases as i increases. \n\nFor example: \n=> When i is 1, (2 * 1 - 1) equals 1 star =>      * \n=> When i is 2, (2 * 2 - 1) equals 3 stars =>    *** \n=> When i is 3, (2 * 3 - 1) equals 5 stars =>   ***** \n=> When i is 4, (2 * 4 - 1) equals 7 stars =>  ******* \n=> When i is 5, (2 * 5 - 1) equals 9 stars => *********")
                                             
-                                    quizQuestion = str(input('\nIn order to keep your learning up-to-date I have arrange a quiz for you in which you have to create \nthe Pattern of Equilateral-Triangle by yourself. Do you want to attend the quiz (yes/no): '))
+                                    quizQuestion = str(input('\nIn order to keep your learning up-to-date I have arrange a quiz for you in which you have to create \nthe Pattern of Pyramid-Triangle by yourself. Do you want to attend the quiz (yes/no): '))
                                 
                                     if quizQuestion == 'yes':
                                         
@@ -78,7 +78,7 @@ if answerQuestion1 == 'yes':
                                             
                                             printStatement = str(input('    '))
                                             
-                                            if printStatement == "print('' * (5 - i) + '*' * (2 * i - 1))":
+                                            if printStatement == "print(' ' * (5 - i) + '*' * (2 * i - 1))":
                                             
                                                 print('\nOutput :- \n    * \n   *** \n  ***** \n ******* \n********* \n\nGreat Achievement! You got (5/5), Keep it up!')
                                             
