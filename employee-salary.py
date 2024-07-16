3be51df9c145a19b42ce47c5c8414e95b528607a
tayyab_20 <tayyabali78013@gmail.com>
2024-07-16 23:07:44 +0500

    first commit

---
+++ employee-salary.py
@@ -0,0 +1,5 @@
+employee_name = str(input('Dear please enter your name here: '))
+
+employee_salary = int(input(f'Dear {employee_name}, please enter your salary here: '))
+
+print(f'Dear {employee_name}, your salary is {employee_salary} pkr.')
