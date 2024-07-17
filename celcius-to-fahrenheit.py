a1d80a6dde946032ca0e9d3bcabd0f81763707d8
tayyab_20 <tayyabali78013@gmail.com>
2024-07-17 16:10:06 +0500

    first commit

--- celcius-to-fahrenheit.py
+++ celcius-to-fahrenheit.py
@@ -2,4 +2,4 @@ celcius = int(input('Enter the celcius rate to convert to fahrenheit: '))
 
 fahrenheit = ( 9 / 5 * celcius) + 32
 
-print(f'\nThe conversion of Celcius {50}{chr(176)} Degree to Fahrenheit is = {fahrenheit}{chr(176)} Degree')
+print(f'\nThe conversion of Celcius {celcius}{chr(176)} Degree to Fahrenheit is = {fahrenheit}{chr(176)} Degree.')
