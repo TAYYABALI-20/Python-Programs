0910b427bc3e72c86d9a3e51c4936a13dfc413b7
tayyab_20 <tayyabali78013@gmail.com>
2024-08-11 00:53:34 +0500

    first commit

---
+++ format-date.py
@@ -0,0 +1,53 @@
+# Format the date
+
+date = input("Enter today's date (mm/dd/yyyy): ")
+
+month, day, year = date.split('/')
+
+if month == '01':
+    month = 'January'
+    print("\nToday's date:",month, day+ ', ' + year)
+
+if month == '02':
+    month = 'Febuary'
+    print("\nToday's date:",month, day+ ', ' + year)
+
+if month == '03':
+    month = 'March'
+    print("\nToday's date:",month, day+ ', ' + year)
+
+if month == '04':
+    month = 'April'
+    print("\nToday's date:",month, day+ ', ' + year)
+
+if month == '05':
+    month = 'May'
+    print("\nToday's date:",month, day+ ', ' + year)
+
+if month == '06':
+    month = 'June'
+    print("\nToday's date:",month, day+ ', ' + year)
+
+if month == '07':
+    month = 'July'
+    print("\nToday's date:",month, day+ ', ' + year)
+
+if month == '08':
+    month = 'August'
+    print("\nToday's date:",month, day+ ', ' + year)
+
+if month == '09':
+    month = 'September'
+    print("\nToday's date:",month, day+ ', ' + year)
+
+if month == '10':
+    month = 'October'
+    print("\nToday's date:",month, day+ ', ' + year)
+
+if month == '11':
+    month = 'November'
+    print("\nToday's date:",month, day+ ', ' + year)
+
+if month == '12':
+    month = 'December'
+    print("\nToday's date:",month, day+ ', ' + year)
